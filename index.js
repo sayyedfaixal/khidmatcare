b0c4511b1e12143a13a625eb95e00c1472d16d88
Faisal Sayyed <sayedfaisal544@gmail.com>
2021-06-04 17:48:04 +0530

    fixed all bugs

---
+++ index.js
@@ -0,0 +1,38 @@
+var state = document.querySelector("#states");
+var house = document.querySelector("#house");
+var people = document.querySelector("#people");
+
+let incState =1;
+setInterval(() => {
+    if(incState<=24){
+        incState++;
+    state.innerHTML = incState;
+    }
+}, 200);
+
+
+let incHouse = 1;
+setInterval(() => {
+    if(incHouse <50){
+        incHouse++;
+        house.innerHTML = incHouse;
+    }
+}, 100);
+
+let incPeople = 1;
+setInterval(() => {
+    if(incPeople<=999){
+        incPeople ++;
+        people.innerHTML = incPeople;
+    }
+}, 0.2 );
+//Onclick about us will take to the footer
+
+function scrollDown(){
+    window.scroll({
+              top: document.body.scrollHeight,
+              behavior: 'smooth'
+          });
+  }
+
+var donateNow  = document.querySelector('.donate-now');
